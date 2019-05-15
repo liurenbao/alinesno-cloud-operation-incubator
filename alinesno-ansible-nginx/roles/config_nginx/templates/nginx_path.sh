eb1ab1ef2b528560203c949acbcdef09f0c81670
LuoAnDong <luoandon@gmail.com>
2019-05-15 22:33:09 +0800

    添加nginx安装教程。

--- alinesno-ansible-nginx/roles/config_nginx/templates/nginx_path.sh
+++
@@ -1,2 +0,0 @@
-export NGINX_PATH={{install_dir}}/nginx-{{nginx_version}}/sbin
-export PATH=$NGINX_PATH:$PATH
--- alinesno-ansible-nginx/roles/install_nginx/templates/configure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-config_line="{%for line in config_list %}{{line}} {%endfor%}"
-cd {{download_dir}}/temp/nginx-{{nginx_version}}
-./configure $config_line
