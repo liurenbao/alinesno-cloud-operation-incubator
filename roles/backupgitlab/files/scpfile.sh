28c3ce130273c67eab86051696a832d630c6a505
苏 泽 <suze9112102313@163.com>
2021-04-20 16:03:30 +0000

    Update scpfile.sh

--- roles/backupgitlab/files/scpfile.sh
+++ roles/backupgitlab/files/scpfile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-spawn scp $1 root@47.115.42.99:/root/backup/gitlab/
+spawn scp -r /var/opt/gitlab/backups/ root@47.115.42.99:/root/backup/gitlab/
   expect {
     "password:" { send "Xinhu\@1234qwer\r" }
     "yes/no" { send "yes\r" }
